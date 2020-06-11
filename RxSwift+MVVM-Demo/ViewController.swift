144c27702e41d2c6c3db68de803540999eb59606
Miguel <gannasong@gmail.com>
2020-06-12 00:01:16 +0800

    init project

---
+++ RxSwift+MVVM-Demo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM-Demo
+//
+//  Created by SUNG HAO LIN on 2020/6/11.
+//  Copyright © 2020 SUNG HAO LIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
---
+++ RxSwift+MVVM-DemoTests/RxSwift_MVVM_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxSwift_MVVM_DemoTests.swift
+//  RxSwift+MVVM-DemoTests
+//
+//  Created by SUNG HAO LIN on 2020/6/11.
+//  Copyright © 2020 SUNG HAO LIN. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_MVVM_Demo
+
+class RxSwift_MVVM_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
